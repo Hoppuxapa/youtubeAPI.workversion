0e929e639510d19e7ec9e5c61ace7b7501b71740
illiagitdev <illya.posv.b@gmail.com>
2020-01-23 18:25:45 +0200

    deleted extra imports

--- src/main/java/com/youtube/ui/layout/ChannelPanel.java
+++ src/main/java/com/youtube/ui/layout/ChannelPanel.java
@@ -1,38 +1,22 @@
 package com.youtube.ui.layout;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.youtube.controls.search.Search;
 import com.youtube.response.SimpleResponse;
 import com.youtube.response.parcer.ApiResponse;
-import com.youtube.response.parcer.items.Items;
-import com.youtube.response.parcer.items.Thumbnails;
+import com.youtube.response.parcer.items.*;
 import com.youtube.ui.components.ImageLoader;
-import com.youtube.ui.components.view.Channel;
 import com.youtube.ui.components.view.Video;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Font;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import okhttp3.HttpUrl;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import javafx.scene.layout.*;
+import okhttp3.*;
 
-import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,6 +25,21 @@ import java.util.Objects;
 import static com.youtube.config.HttpConfig.KEY;
 
 public class ChannelPanel {
+    TextField searchText = new TextField("Enter text");
+    Button searchChannel = new Button("Search");
+    private String channelName;
+    private String channelDescription;
+    private String urlIDChannel;
+    private String imageUrl;
+
+    public ChannelPanel(String channelName, String channelDescription, String urlIDChannel, String imageUrl) {
+        System.out.println("New channel created!! " + Thread.currentThread().getName()
+                + " channelID: " + urlIDChannel);
+        this.channelName = channelName;
+        this.channelDescription = channelDescription;
+        this.urlIDChannel = urlIDChannel;
+        this.imageUrl = imageUrl;
+    }
 
     public void searchFromChannel(String searchText, ListView<GridPane> listView) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
@@ -84,15 +83,13 @@ public class ChannelPanel {
 
             }
         }
-        // return either objects for general search or channel search
-        // current solution
-        //todo: make one adaptive class
+
         List<GridPane> sample = new ArrayList<>();
         for (SimpleResponse searchResult : searchResults) {
             sample.add(new Video(searchResult).newList());
         }
 
-        //make task run later in main FX thread save from - "IllegalStateException: Not on FX application thread"
+        //make task run later in main FX thread
         ObservableList<GridPane> observableList = FXCollections.observableList(sample);
         Platform.runLater(() -> listView.setItems(observableList));
     }
@@ -101,41 +98,26 @@ public class ChannelPanel {
         String url;
         if (thumbnails.getRandom() != null) {
             url = thumbnails.getRandom().getUrl();
-            System.out.println("thumbnails.getStandard().getUrl() = " + url +
-                    " || getFirstUrl: " + thumbnails.getClass().getSimpleName());
             return url;
         } else if (thumbnails.getMedium() != null) {
             url = thumbnails.getMedium().getUrl();
-            System.out.println("thumbnails.getStandard().getUrl() = " + url +
-                    " || getFirstUrl: " + thumbnails.getClass().getSimpleName());
             return url;
         } else if (thumbnails.getStandard() != null) {
             url = thumbnails.getStandard().getUrl();
-            System.out.println("thumbnails.getStandard().getUrl() = " + url +
-                    " || getFirstUrl: " + thumbnails.getClass().getSimpleName());
             return url;
         } else if (thumbnails.getHigh() != null) {
             url = thumbnails.getHigh().getUrl();
-            System.out.println("thumbnails.getStandard().getUrl() = " + url +
-                    " || getFirstUrl: " + thumbnails.getClass().getSimpleName());
             return url;
         } else if (thumbnails.getMaxres() != null) {
             url = thumbnails.getMaxres().getUrl();
-            System.out.println("thumbnails.getStandard().getUrl() = " + url +
-                    " || getFirstUrl: " + thumbnails.getClass().getSimpleName());
             return url;
         }
         return "https://i.ytimg.com/vi/yWpKll3G_a0/default.jpg";
     }
 
-
     public TextField getSearchText() {
         return searchText;
     }
-
-    TextField searchText = new TextField("Enter text");
-    Button searchChannel = new Button("Search");
-
 
     private void initUI() {
         searchText.setPrefWidth(300);
@@ -145,23 +127,7 @@ public class ChannelPanel {
         searchChannel.setText("Search");
     }
 
-    private String channelName;
-    private String channelDescription;
-    private String urlIDChannel;
-    private String imageUrl;
-
-    public ChannelPanel(String channelName, String channelDescription, String urlIDChannel, String imageUrl) {
-        System.out.println("New channel created!! " + Thread.currentThread().getName()
-                + " channelID: " + urlIDChannel);
-        this.channelName = channelName;
-        this.channelDescription = channelDescription;
-        this.urlIDChannel = urlIDChannel;
-        this.imageUrl = imageUrl;
-    }
-
-    //todo: do nice layout - userUI
     public Pane newChannelPane() {
-
         VideoList channelView = new VideoList(1000, 800, 210);
 
         Label name = new Label(channelName);
@@ -176,8 +142,6 @@ public class ChannelPanel {
         description.setEditable(false);
         VBox channelInfo = new VBox(name, description);
 
-
-
         searchChannel.setOnMouseClicked(event -> {
             try {
                 searchFromChannel(searchText.getText(),channelView.getResultsList());
@@ -186,7 +150,6 @@ public class ChannelPanel {
             }
         });
 
-//        ImageView channelImage = new ImageView(new Image("https://i.ytimg.com/vi/yWpKll3G_a0/default.jpg"));
         ImageView image = new ImageView();
         image.setTranslateX(10);
         searchText.setTranslateX(30);
@@ -195,7 +158,6 @@ public class ChannelPanel {
         header.setTranslateY(5);
 
         //loadImage:
-        System.out.println("URL for images - " + imageUrl + " | " + this.getClass().getSimpleName());
         new Thread(new ImageLoader(image, imageUrl)).start();
 
         //handle content: fill last videos
@@ -205,7 +167,5 @@ public class ChannelPanel {
         Pane pane = new Pane();
         pane.getChildren().addAll(header, channelView.getResultsBox());
         return pane;
-
-
     }
 }
